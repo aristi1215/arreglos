13f7f2071b4f5c684239810e76ea597f64cc4fbc
unknown <juan pablo aristizabal>
2022-04-20 17:01:42 -0500

    controlador.js

---
+++ controlador.js
@@ -0,0 +1,12 @@
+let numeros = Array (1,2,3,4,5,6,7,8,9,10)
+console.log (numeros)
+console.log (numeros [7] )
+let nombres = Array ("juan","fernando","miguel","sebastian","aristi","mateo","julian","jose","sara","fernanda")
+console.log (nombres)
+console.log (nombres [8])
+let edades = Array (10,20,30,40,50,60,70,80,90,100)
+console.log (edades)
+for (let i=0; i<nombres.length; i++) {
+console.log (nombres [i]+edades[i])
+
+}
